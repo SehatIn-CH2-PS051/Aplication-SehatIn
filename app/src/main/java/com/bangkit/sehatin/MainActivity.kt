614a83c9941050c834ea08db2fc30944f8de1004
MuhammadSadiqin <msadiqin46@gmail.com>
2023-12-20 23:04:59 +0700

    baru

--- app/src/main/java/com/bangkit/sehatin/MainActivity.kt
+++ app/src/main/java/com/bangkit/sehatin/MainActivity.kt
@@ -25,4 +25,5 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, AddFoodActivity::class.java)
         startActivity(intent)
     }
+
 }
--- app/src/main/java/com/bangkit/sehatin/data/network/retrofit/ApiService.kt
+++ app/src/main/java/com/bangkit/sehatin/data/network/retrofit/ApiService.kt
@@ -4,9 +4,11 @@ import com.bangkit.sehatin.LoginData
 import com.bangkit.sehatin.RegistrationData
 import com.bangkit.sehatin.data.network.response.FoodResponse
 import okhttp3.MultipartBody
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
@@ -18,10 +20,15 @@ interface ApiService {
     @POST("/login")
     fun logIn(@Body loginData: LoginData): Call<Response<String>>
 
+    @GET("/user")
+    fun getUserInfo(): Call<ResponseBody>
+
     @Multipart
     @POST("/food")
     suspend fun uploadImage(
         @Part file: MultipartBody.Part,
     ): FoodResponse
+
+
 
 }
